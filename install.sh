37e90ea91564075af2c8a5bee75d4b3517e2d7a1
laciferin2024 <laciferin@gmail.com>
2024-11-30 06:40:36 +0000

    main: d50e9b2

--- install.sh
+++ install.sh
@@ -78,7 +78,7 @@ install_darts() {
   cp $DARTS_LOC $DARTS_INSTALL_LOC 
 
   if command -v darts >/dev/null 2>&1; then
-    echo "Installed darts successfully!"
+    echo "Installed darts successfully!" in "$DARTS_INSTALL_LOC"
   else
     echo "Darts installation failed or not found in /usr/local/bin"
   fi
