242518ccdffefec4b943daed9425e9e5a38ca12b
laciferin2024 <laciferin@gmail.com>
2024-12-08 06:49:55 +0000

    main: ad79caf

--- install.sh
+++ install.sh
@@ -53,7 +53,7 @@ detect_os_info() {
 
 install_darts() {
   DARTS_LOC=${DARTS_LOC:-"/tmp/darts-binary"}
-  DARTS_INSTALL_LOC=${DARTS_INSTALL_LOC:-"/usr/local/bin"}
+  DARTS_INSTALL_LOC=${DARTS_INSTALL_LOC:-"/usr/local/bin/darts"}
 
   getLatestRelease
   echo "installing darts:$version"
